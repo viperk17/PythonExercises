e598be9d6169d79a3aadbb44ed38c6e306dce73e
Prashant Singh <pk.airsqn@gmail.com>
2020-05-24 23:24:28 +0530

    Update Find the divisior of a number.py

--- Find the divisior of a number.py
+++ Find the divisior of a number.py
@@ -4,9 +4,6 @@ Create a program that asks the user for a number and then prints out a list of a
 For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
 """
 
-
-__author__ = 'viper'
-
 num = int(input("Please choose a number to divide: "))
 listRange = list(range(1,num+1))
 
