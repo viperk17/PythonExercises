e5743efb4afda81fe8f238860d1cde5a2b7574b9
Prashant Singh <pk.airsqn@gmail.com>
2020-05-25 11:23:13 +0530

    Create Print ASCII Value.py

---
+++ Print ASCII Value.py
@@ -0,0 +1,7 @@
+# print ASCII Value of a character
+'''ord() : It coverts the given string of length one, return an integer representing 
+the unicode code point of the character. For example, ord(‘a’) returns the integer 97.'''
+
+c = 'A'
+
+print("The ASCII value of '" + c + "' is", ord(c)) 
