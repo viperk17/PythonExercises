8daa185826bf1a4e523f56512fd6cc2d1eb12585
Prashant Singh <pk.airsqn@gmail.com>
2020-06-03 20:44:40 +0530

    Update FInd a string.py

--- FInd a string.py
+++ FInd a string.py
@@ -17,6 +17,5 @@ def count_substring(string, sub_string):
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
-    
     count = count_substring(string, sub_string)
     print(count)
